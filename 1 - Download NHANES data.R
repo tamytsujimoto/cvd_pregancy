78c21231334982b4b25aaa938fab888478272e70
Tamy Tsujimoto <tamy.tsujimoto@gmail.com>
2019-05-08 14:11:32 -0400

    Variable extraction

--- 1 - Download NHANES data.R
+++ 1 - Download NHANES data.R
@@ -8,43 +8,31 @@ library(tidyverse)
 yr = paste(seq(1999, 2013, by = 2), seq(2000, 2014, by = 2), sep = "-")
 
 # Demographic Variables & Sample Weights
-
 nhanes_load_data(file = "DEMO", 
                  year = yr, 
                  destination = "nhanes_data")
 
 # Blood Pressure
-
 nhanes_load_data(file = "BPX", 
                  year = yr, 
                  destination = "nhanes_data")
 
 # Blood Pressure & Cholesterol
-
 nhanes_load_data(file = "BPQ", 
                  year = yr, 
                  destination = "nhanes_data")
 
 # Smoking - Cigarette/Tobacco Use - Adult 
-
-nhanes_load_data(file = "SMQ", 
-                 year = yr, 
-                 destination = "nhanes_data")
-
-# Smoking - Cigarette/Tobacco Use - Adult 
-
 nhanes_load_data(file = "SMQ", 
                  year = yr, 
                  destination = "nhanes_data")
 
 # Diabetes
-
 nhanes_load_data(file = "DIQ", 
                  year = yr, 
                  destination = "nhanes_data")
 
 # Reproductive Health 
-
 nhanes_load_data(file = "RHQ", 
                  year = yr, 
                  destination = "nhanes_data")
@@ -54,7 +42,6 @@ nhanes_load_data(file = "RHQ",
 ########################################
 
 # Cholesterol 
-
 nhanes_load_data(file = c('Lab13', 'l13_B', 'l13_C', rep('TCHOL', 5)), 
                  year = yr, 
                  destination = "nhanes_data")
@@ -64,13 +51,11 @@ nhanes_load_data(file = 'HDL',
                  destination = "nhanes_data")
 
 # Cotinine 
-
 nhanes_load_data(file = c('LAB06', 'L06_B', 'L06COT_C', 'COT_D', 'COTNAL_E', 'COTNAL_F', 'COTNAL_G', 'COT_H'), 
                  year = yr, 
                  destination = "nhanes_data")
 
 # Glycohemoglobin 
-
 nhanes_load_data(file = c('LAB10', 'L10_B', 'L10_C', rep('GHB', 5)), 
                  year = yr, 
                  destination = "nhanes_data")
---
+++ 2 - NHANES variable extraction.R
@@ -0,0 +1,206 @@
+library(tidyverse)
+
+###################################
+# SELECTING VARIABLES OF INTEREST #
+###################################
+
+# Path for the downloaded datasets
+data_path = '../3 - Databases/nhanes_data'
+
+# Function to stack NHANES dataset
+stack_nhanes_data = function(path = data_path, pattern){
+  
+  data_names = list.files(path = path, pattern = pattern)
+  
+  db = 
+    lapply(paste(path, data_names, sep = '/'), data.table::fread) %>% 
+    bind_rows()
+  
+  return(db)
+}
+
+# Stacking nhanes datasets and selecting variables of interest:
+
+# Demographic Variables & Sample Weights
+demo = 
+  stack_nhanes_data(pattern = 'DEMO') %>% 
+  select(SEQN,
+         file_name,
+         cycle,
+         begin_year,
+         end_year,
+         RIAGENDR,
+         RIDRETH1,
+         RIDRETH2,
+         RIDRETH3,
+         RIDAGEYR,
+         RIDAGEMN,
+         RIDAGEEX,
+         RIDEXPRG,
+         WTINT2YR,
+         WTMEC2YR)
+
+# Blood Pressure
+bpx = 
+  stack_nhanes_data(pattern = 'BPX') %>% 
+  select(SEQN,
+         file_name,
+         cycle,
+         begin_year,
+         end_year,
+         BPXSY1,
+         BPXSY2,
+         BPXSY3,
+         BPXSY4,
+         BPXSAR,
+         BPXDI1,
+         BPXDI2,
+         BPXDI3,
+         BPXDI4,
+         BPAEN1,
+         BPAEN2,
+         BPAEN3,
+         BPAEN4)
+
+# Blood Pressure & Cholesterol
+bpq = 
+  stack_nhanes_data(pattern = 'BPQ') %>% 
+  select(SEQN,
+         file_name,
+         cycle,
+         begin_year,
+         end_year,
+         BPQ080,
+         BPQ020,
+         BPQ030,
+         BPQ040A,
+         BPQ050A,
+         BPQ100D)
+
+# Smoking - Cigarette/Tobacco Use - Adult 
+smq = 
+  stack_nhanes_data(pattern = 'SMQ') %>% 
+  select(SEQN,
+         file_name,
+         cycle,
+         begin_year,
+         end_year,
+         SMQ020,
+         SMQ040,
+         SMQ050Q,
+         SMQ050U)
+
+# Diabetes  
+diq = 
+  stack_nhanes_data(pattern = 'DIQ') %>% 
+  select(SEQN,
+         file_name,
+         cycle,
+         begin_year,
+         end_year,
+         DIQ010,
+         DIQ050,
+         DIQ070,
+         DID070,
+         DIQ280)
+
+# Reproductive Health          
+rhq = 
+  stack_nhanes_data(pattern = 'RHQ') %>% 
+  select(SEQN,
+         file_name,
+         cycle,
+         begin_year,
+         end_year,
+         RHQ031,
+         RHD042,
+         #RHQ135C,
+         RHD152,
+         #RHQ165,
+         RHQ166,
+         RHQ169,
+         RHQ171,
+         #RHQ175,
+         RHQ180,
+         RHQ190,
+         #RHQ195,
+         RHD130,
+         RHQ131,
+         RHQ140,
+         RHQ141,
+         RHD143,
+         RHQ160,
+         RHD170,
+         RHQ171,
+         RHD270,
+         RHQ250,
+         RHQ260,
+         RHQ210,
+         RHD230,
+         RHQ162)
+
+# Cholesterol 
+chol = 
+  stack_nhanes_data(pattern = paste(c('Lab13', 'l13_b', 'l13_c', 'TCHOL'), collapse = "|")) %>% 
+  select(SEQN,
+         file_name,
+         cycle,
+         begin_year,
+         end_year,
+         LBXTC,
+         LBDHDL,
+         LBXHDD)
+
+# HDL 
+hdl = 
+  stack_nhanes_data(pattern ='HDL') %>% 
+  select(SEQN,
+         file_name,
+         cycle,
+         begin_year,
+         end_year,
+         LBDHDD)
+
+# Cotinine 
+cot = 
+  stack_nhanes_data(pattern = paste(c('LAB06', 'L06_B', 'L06COT_C', 'COT_D', 'COTNAL_E', 'COTNAL_F', 'COTNAL_G', 'COT_H'), collapse = "|")) %>% 
+  select(SEQN,
+         file_name,
+         cycle,
+         begin_year,
+         end_year,
+         LBXCOT)
+
+# Glycohemoglobin 
+glyc = 
+  stack_nhanes_data(pattern = paste(c('LAB10', 'L10_B', 'L10_C', 'GHB'), collapse = "|")) %>% 
+  select(SEQN,
+         file_name,
+         cycle,
+         begin_year,
+         end_year,
+         LBXGH)
+
+############
+# CHECKING #
+############
+
+bind_rows('demo' = demo, 
+            'bpx' = bpx, 
+            'bpq' = bpq, 
+            'smq' = smq,
+            'diq' = diq,
+            'rhq' = rhq, 
+            'chol' = chol,
+            'hdl' = hdl,
+            'cot' = cot, 
+            'glyc' = glyc, 
+            .id = 'db_name') %>% 
+  select(db_name, cycle) %>% 
+  group_by(db_name, cycle) %>% 
+  summarise(n = n()) %>% 
+  spread(key = cycle, value = n) %>% 
+  ungroup() %>% 
+  mutate(total = rowSums(.[,2:9])) %>% 
+  arrange(desc(total))
+
